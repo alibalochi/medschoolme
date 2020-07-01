d02a325436c15134e87ad186ed1e2e995b28f357
Ali Baloch <ali.haider.baloch@gmail.com>
2020-07-01 13:46:35 +0100

    Updates

---
+++ precache-manifest.dcb4a0bd8c1318ff09ad543fb59416ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaed023cb406b1bab954197ad10848a9",
+    "url": "/medschoolme/index.html"
+  },
+  {
+    "revision": "decad0f982bfa57213cd",
+    "url": "/medschoolme/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8e4e237f0e704b6095f6",
+    "url": "/medschoolme/static/css/main.d8bad536.chunk.css"
+  },
+  {
+    "revision": "decad0f982bfa57213cd",
+    "url": "/medschoolme/static/js/2.0afa7d55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/medschoolme/static/js/2.0afa7d55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4e237f0e704b6095f6",
+    "url": "/medschoolme/static/js/main.a31543b6.chunk.js"
+  },
+  {
+    "revision": "7e042f17025d0c2d4051",
+    "url": "/medschoolme/static/js/runtime-main.82f2a6ef.js"
+  },
+  {
+    "revision": "1ea0a0571e2aad3abf9895b3918d00f2",
+    "url": "/medschoolme/static/media/fail.1ea0a057.svg"
+  },
+  {
+    "revision": "76bf687857825857f2d1adbee1bb3129",
+    "url": "/medschoolme/static/media/homepage.76bf6878.svg"
+  },
+  {
+    "revision": "340e2f81f0d38d4f2e951953aead69f5",
+    "url": "/medschoolme/static/media/pass.340e2f81.svg"
+  }
+]);
